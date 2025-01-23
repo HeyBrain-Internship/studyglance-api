7a831d304bb018861d8003ab32d8af3b3b34858e
Elektroair <elektroair.aero@gmail.com>
2025-01-23 16:38:34 +0000

    Fix of GET requests

--- src/index.ts
+++ src/index.ts
@@ -9,27 +9,11 @@ fastify.register(cors,
                      origin : "*"
                  })
 
-fastify.get('/', (request, response) =>
+fastify.get('/get', async (request, response) =>
 {
-    const body = request.body;
-    console.log(body);
-    response.send({ status : "OK", receivedData : body });
+    const data = await getSheetData();
+    response.send({ status : "OK", receivedData : data });
 })
 
-const startServer = async () =>
-{
-    try
-    {
-        await fastify.listen({ port : 8080 });
-        console.log("Listening on port 8080");
-    }
-    catch (error)
-    {
-        console.error(error);
-        process.exit(1);
-    }
-}
-
-startServer().then(response => console.log(response));
-
-fastify.register(getSheetData, { suffix : "/get"});
+void fastify.listen({ port : 8080 });
+console.log("Listening on port 8080");
