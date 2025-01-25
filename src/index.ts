a569697de61f675a137c9aa3d9c52b17cd385d33
Elektroair <elektroair.aero@gmail.com>
2025-01-25 20:06:42 +0000

    Debug of server response

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ fastify.get('/get', async (request, response) =>
 {
     console.log("Server response:   " + response);
     const data = await getSheetData();
-    console.log("Server response data:   " + data);
+    console.log("Server data:   " + data);
     response.send({ status : "OK", receivedData : data });
 })
 
