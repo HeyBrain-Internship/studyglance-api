f0bd3de17bb0e0a7a6cd27f52c3f424f3d10f22f
Elektroair <elektroair.aero@gmail.com>
2025-01-25 21:01:07 +0000

    Debug of server response

--- src/google/authentication.ts
+++ src/google/authentication.ts
@@ -1,31 +1,39 @@
 import { google } from "googleapis";
 
-const SERVICE_ACCOUNT_FILE = "./src/google/studyglance-service.json";
-
 async function authenticate()
 {
 
     try
     {
-        const authentication = new google.auth.GoogleAuth({
-                                                              credentials :
-                                                                       {
-                                                                           client_email : process.env.GOOGLE_CLIENT_EMAIL,
-                                                                           private_key :  process.env.GOOGLE_PRIVATE_KEY
-                                                                       },
-                                                              scopes : ["https://www.googleapis.com/auth/spreadsheets.readonly"]
-                                                          });
-
+        const authentication = new google.auth.GoogleAuth(
+            {
+                credentials :
+                    {
+                        client_email : process.env.GOOGLE_CLIENT_EMAIL,
+                        private_key :  process.env.GOOGLE_PRIVATE_KEY!.replace(/\\n/g, "\n")
+                    },
+                scopes : ["https://www.googleapis.com/auth/spreadsheets.readonly"]
+            });
 
 
         const authenticatedClient = await authentication.getClient();
-        console.log("Google Authentication response:   " + authenticatedClient);
+
         // @ts-ignore
-        return google.sheets({ version : 'v4', auth : authenticatedClient });
+        const response = google.sheets({ version : 'v4', auth : authenticatedClient });
+
+        if (response)
+        {
+            console.log("Google Sheets request response:   " + response);
+        }
+        else
+        {
+            console.log("Google Sheets request error:   " + response);
+        }
+        return response;
     }
-    catch(error)
+    catch (error)
     {
-        console.log("Google Authentication error:   " + error);
+        console.log("Google Authentication/Sheets error:   " + error);
     }
 }
 
